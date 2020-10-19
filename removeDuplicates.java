c231e05416be0d788aa1abb8ffc7036b868861fa
kyleyiyang <kyleyiyang@gmail.com>
2020-10-19 16:42:10 -0400

    Update removeDuplicates.java

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -1,17 +1,14 @@
-public class HelloWorld{
-
-     public static void main(String []args){
-        System.out.println("Hello World");
-        String A = "abbacaaaaaadaaaaa";
+class Solution {
+    public String removeDuplicates(String S) {
         int i=0;
-        while (i<A.length()-1) {
-            if (A.charAt(i+1)==A.charAt(i)) {
-                A = A.substring(0, i) + A.substring(i+2);
+        while (i<S.length()-1) {
+            if (S.charAt(i+1)==S.charAt(i)) {
+                S = S.substring(0, i) + S.substring(i+2);
                 i=0;
             } else {
                 i++;
             }
         }
-        System.out.println(A);
-     }
+        return S;
+    }
 }
